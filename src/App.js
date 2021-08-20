1dfe3bad41af5e366f0c81bb75ec8821b9c2214d
Atle Weibell <atle@weibell.no>
2021-08-21 01:09:31 +0200

    i18next: Proof of concept. Bumped package versions.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useTranslation, Trans} from "react-i18next";
+import { languages } from './i18n-config';
 
 function App() {
+    const { t, i18n } = useTranslation();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <div>
+                {Object.keys(languages).map(lngCode => (
+                    <button key={lngCode} type="submit" onClick={() => i18n.changeLanguage(lngCode)}>
+                        {languages[lngCode].nativeName}
+                    </button>
+                ))}
+            </div>
+            <Trans i18nKey="header.title">
+                Header content here
+            </Trans>
+        </header>
+        <nav>
+            {t('navigation.title')}
+        </nav>
+        <section>
+            {t('section.placeholder')}
+        </section>
+        <aside>
+            {t('aside.placeholder')}
+        </aside>
+        <footer>
+            {t('footer.placeholder')}
+        </footer>
     </div>
   );
 }
---
+++ src/i18n-config.js
@@ -0,0 +1,62 @@
+export const languages = {
+    en: {
+        nativeName: 'English',
+    },
+    'nn-NO': {
+        nativeName: 'Norsk - nynorsk',
+    },
+    se: {
+        nativeName: 'Svenska',
+    },
+};
+
+export const resources = {
+    en: {
+        translation: {
+            header: {
+                title: 'Header needs some content'
+            },
+            navigation: {
+                title: 'Navigation goes here'
+            },
+            section: {
+                placeholder: 'this will be the main section',
+            },
+            aside: {
+                placeholder: 'A little sidebar here, perhaps?',
+            },
+            footer: {
+                placeholder: 'Finally the footer',
+            }
+            // here we will place our English translations...
+        }
+    },
+    'nn-NO': {
+        translation: {
+            header: {
+                title: 'Her kjem innhaldet i toppen'
+            },
+            navigation: {
+                title: 'Navigering dukkar opp her'
+            },
+            section: {
+                placeholder: 'Hovudinnhaldet vil koma inn her',
+            },
+            aside: {
+                placeholder: 'Ein side-meny her?',
+            },
+            footer: {
+                placeholder: 'Nedst kjem ei avslutning',
+            }
+            // here we will place our Norwegian translations...
+        }
+    },
+    se: {
+        translation: {
+            // here we will place our Swedish translations...
+            navigation: {
+                title: 'Här kommer menyn'
+            }
+        }
+    },
+};
